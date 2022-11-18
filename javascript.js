3f03e72883e775368d488ea8569942b52699e00c
Kieran <kieran.webdevelopment@gmail.com>
2022-11-18 14:30:23 +0000

    basics html and css done for main card. playing around with js

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// let ratingOption = document.querySelector('.rating-option');
+// let submit = document.getElementById('submit-button');
+
+// function changeColor() {
+//   ratingOption.classList.toggle('orange');
+// }
+
+// console.log('hello');
+// // let testing = getComputedStyle(
+// //   document.getElementsByClassName('rating-option')
+// // );
+
+// // testing.backgroundColor = 'blue';
+// ratingOption.addEventListener('click', changeColor);
