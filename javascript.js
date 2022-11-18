b73548728d74bdba95b157727c1f2c99771950fc
Kieran <kieran.webdevelopment@gmail.com>
2022-11-18 18:09:59 +0000

    added functioning js, although needs refactoring

--- javascript.js
+++ javascript.js
@@ -1,3 +1,45 @@
+let ratingIsOne = document.querySelector('.one');
+let ratingIsTwo = document.querySelector('.two');
+let ratingIsThree = document.querySelector('.three');
+let ratingIsFour = document.querySelector('.four');
+let ratingIsFive = document.querySelector('.five');
+let submit = document.querySelector('#submit-button');
+let ratingMessage = document.querySelector('.rating');
+
+let rating = 0;
+
+function buttonOneSelected(element) {
+  ratingIsOne.classList.remove('orange');
+  ratingIsTwo.classList.remove('orange');
+  ratingIsThree.classList.remove('orange');
+  ratingIsFour.classList.remove('orange');
+  ratingIsFive.classList.remove('orange');
+  element.classList.toggle('orange');
+  element.classList.remove('hover');
+  rating = element.innerText;
+  console.log(rating);
+}
+
+function submitRating() {
+  if (rating === 0) {
+    alert('you must select a rating before submitting');
+  } else {
+    ratingMessage.innerText = `You selected ${rating} out of 5`;
+  }
+}
+
+ratingIsOne.addEventListener('click', () => buttonOneSelected(ratingIsOne));
+
+ratingIsTwo.addEventListener('click', () => buttonOneSelected(ratingIsTwo));
+
+ratingIsThree.addEventListener('click', () => buttonOneSelected(ratingIsThree));
+
+ratingIsFour.addEventListener('click', () => buttonOneSelected(ratingIsFour));
+
+ratingIsFive.addEventListener('click', () => buttonOneSelected(ratingIsFive));
+
+submit.addEventListener('click', submitRating);
+
 // let ratingOption = document.querySelector('.rating-option');
 // let submit = document.getElementById('submit-button');
 
